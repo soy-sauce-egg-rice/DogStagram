eb7dd4bbf1d6caae87137b018a958e70a4295fa3
hwk44 <peos0n@kakao.com>
2023-07-19 22:11:48 +0900

    toEntity 추가

--- BE/src/main/java/com/example/dogstar/dto/MemberDto.java
+++ BE/src/main/java/com/example/dogstar/dto/MemberDto.java
@@ -1,5 +1,7 @@
 package com.example.dogstar.dto;
 
+import com.example.dogstar.domain.Board;
+import com.example.dogstar.domain.Member;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,10 +11,17 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class MemberDto {
+public class MemberDTO {
     private String token;
     private String id;
     private String password;
     private String email;
 
+    public Member toEntity() {
+        return Member.builder()
+                .id(id)
+                .password(password)
+                .email(email)
+                .build();
+    }
 }
