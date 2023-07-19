276f08d47e69f5d749ffcc43e94c935f2e0d1da0
hwk44 <peos0n@kakao.com>
2023-07-19 23:40:51 +0900

    board findAll 메서드 추가

--- BE/src/main/java/com/example/dogstar/service/BoardService.java
+++ BE/src/main/java/com/example/dogstar/service/BoardService.java
@@ -6,13 +6,18 @@ import com.example.dogstar.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor // final OR @NotNull 인 필드 생성자 추가
 @Service
 public class BoardService {
 
     private final BoardRepository boardRepository;
 
-    public Board save(BoardDTO boardDto){
+    public Board save(BoardDTO boardDto) {
         return boardRepository.save(boardDto.toEntity());
+    }
+    public List<Board> boardList() {
+        return boardRepository.findAll();
     }
 }
