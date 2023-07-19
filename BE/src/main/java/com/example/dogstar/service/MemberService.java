a340b7fdfffa4ba2f6930d51bb75d96519209e04
hwk44 <peos0n@kakao.com>
2023-07-19 22:13:10 +0900

    optional return

--- BE/src/main/java/com/example/dogstar/service/MemberService.java
+++ BE/src/main/java/com/example/dogstar/service/MemberService.java
@@ -1,11 +1,13 @@
 package com.example.dogstar.service;
 
 import com.example.dogstar.domain.Member;
+import com.example.dogstar.dto.MemberDTO;
 import com.example.dogstar.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 //@RequiredArgsConstructor
 @Slf4j
@@ -15,7 +17,10 @@ public class MemberService {
     @Autowired
     private MemberRepository memberRepository;
 
-    public Member saveMember(Member member) {
+    public Member saveMember(MemberDTO memberDTO) {
+        // member 객체
+        Member member = memberDTO.toEntity();
+
         if (member == null ||
             member.getId() == null ||
             member.getPassword() == null ||
@@ -25,9 +30,13 @@ public class MemberService {
             throw new RuntimeException("Id already exists");
         }
         if (memberRepository.existsByEmail(member.getEmail())){
-            log.warn("Email already exists {}", member.getEmail());
+            log.warn("Email already exists {}", member.getEmail()); // 실행
             throw new RuntimeException("Email already exists");
         }
         return memberRepository.save(member);
+    }
+
+    public Optional<Member> findMember (Member member) {
+        return memberRepository.findById(member.getId());
     }
 }
