725c35d6b3b0603b43e4aa3ea2110b53acf99df0
hwk44 <peos0n@kakao.com>
2023-07-20 17:51:01 +0900

    findAllBoards

--- BE/src/main/java/com/example/dogstar/controller/BoardController.java
+++ BE/src/main/java/com/example/dogstar/controller/BoardController.java
@@ -2,11 +2,15 @@ package com.example.dogstar.controller;
 
 import com.example.dogstar.domain.Board;
 import com.example.dogstar.dto.BoardDTO;
+import com.example.dogstar.dto.ResponseDTO;
 import com.example.dogstar.service.BoardService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,10 +20,22 @@ public class BoardController {
     private final BoardService boardService;
 
     @PostMapping
-    public ResponseEntity<?> addBoard(@RequestBody BoardDTO boardDto){
-        Board savedBoard = boardService.save(boardDto);
+    public ResponseEntity<?> addBoard(@RequestBody BoardDTO boardDto) {
+        try {
+            if (boardDto == null || boardDto.getMemberId() == null) throw new RuntimeException("check your request");
+            Board savedBoard = boardService.save(boardDto);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(savedBoard);
+        } catch (Exception e) {
+            ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
 
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(savedBoard);
+    @GetMapping // db board 에 있는 모든 게시글 조회
+    public ResponseEntity<?> findAllBoards() {
+        List<Board> boaorList = boardService.findAll();
+        ResponseDTO<Board> response = ResponseDTO.<Board>builder().data(boaorList).build();
+        return ResponseEntity.ok().body(response);
     }
 }
