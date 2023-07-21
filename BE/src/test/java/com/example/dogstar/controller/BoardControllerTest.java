d280f3306fa7df2a38576d5a7cdb2137ef6be40b
hwk44 <peos0n@kakao.com>
2023-07-21 20:24:52 +0900

    test code

--- BE/src/test/java/com/example/dogstar/controller/BoardControllerTest.java
+++ BE/src/test/java/com/example/dogstar/controller/BoardControllerTest.java
@@ -3,6 +3,7 @@ package com.example.dogstar.controller;
 import com.example.dogstar.domain.Board;
 import com.example.dogstar.dto.BoardDTO;
 import com.example.dogstar.repository.BoardRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -90,5 +91,66 @@ class BoardControllerTest {
 
         /* then */
         result.andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @DisplayName("findBoard : 게시물 아이디로 조회")
+    void findBoard() throws Exception {
+        /* given */
+        final String url = "/board/{id}";
+        final String memberId = "member1";
+        final String image = "image1";
+        final String content = "content1";
+        final BoardDTO boardDto = new BoardDTO(memberId, image, content);
+
+        // dto -> entity -> json 으로 직렬화
+//        final String requestBody = objectMapper.writeValueAsString(boardDto.toEntity());
+
+        Board savedBoard = boardRepository.save(boardDto.toEntity());
+
+        /* when */
+        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.get(url,savedBoard.getId()));
+
+        /* then */
+        result.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.board.memberId").value(memberId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.board.image").value(image))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.board.content").value(content));
+    }
+
+    @Test
+    @DisplayName("deleteBoard : 글 삭제")
+    void deleteBoard() {
+    }
+
+    @Test
+    @DisplayName("updateBoard : 글 수정")
+    void updateBoard() throws Exception {
+        /* given */
+        final String url = "/board/{id}";
+        final String memberId = "member1";
+        final String image = "image1";
+        final String content = "content1";
+        final BoardDTO boardDto = new BoardDTO(memberId, image, content);
+
+        Board savedBoard = boardRepository.save(boardDto.toEntity());
+
+        final String newImage = "new image";
+        final String newContent = "new content";
+
+        BoardDTO updateRequest = new BoardDTO(memberId,newImage, newContent);
+
+        // dto -> entity -> json 으로 직렬화
+        final String requestBody = objectMapper.writeValueAsString(updateRequest.toEntity());
+        /* when */
+        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.put(url,savedBoard.getId())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody));
+
+        /* then */
+        result.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.board.memberId").value(memberId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.board.image").value(newImage))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.board.content").value(newContent));
     }
 }
